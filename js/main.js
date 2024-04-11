21633b2bdcd7c56e8427990c567e58d70272cba3
YourFullName <kirilltatnin@gmail.com>
2024-04-11 22:56:15 +0300

    сделал измениения страницы

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// page_all-seasons
+
+
+// увеличение
+// let imgHovers = document.querySelectorAll(".all-seasons__hover");
+
+// imgHovers.forEach((value) => {
+//   console.log();
+//   value.addEventListener("mouseover", function () {
+//     value.style.transform = "scale(1.2)";
+//   });
+
+//   value.addEventListener("mouseout", function () {
+//     value.style.transform = "scale(1)";
+//   });
+// });
