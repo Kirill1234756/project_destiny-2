7b8b04283a090ea4bd9bed9773937ef0b0c2ed60
YourFullName <kirilltatnin@gmail.com>
2024-04-18 19:32:59 +0300

    add header

--- js/main.js
+++ js/main.js
@@ -1,16 +1,2 @@
-// page_all-seasons
 
 
-// увеличение
-// let imgHovers = document.querySelectorAll(".all-seasons__hover");
-
-// imgHovers.forEach((value) => {
-//   console.log();
-//   value.addEventListener("mouseover", function () {
-//     value.style.transform = "scale(1.2)";
-//   });
-
-//   value.addEventListener("mouseout", function () {
-//     value.style.transform = "scale(1)";
-//   });
-// });
